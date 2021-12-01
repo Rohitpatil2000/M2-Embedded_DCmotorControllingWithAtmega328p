a854cae2c396c4a0f4ebc01a4d0821045d98a305
Rohitpatil2000 <94243541+Rohitpatil2000@users.noreply.github.com>
2021-12-01 23:02:44 +0530

    Update dcmotor.c

--- 3_Implementation/dcmotor.c
+++ 3_Implementation/dcmotor.c
@@ -26,3 +26,31 @@ PORTC=0x01;
   return 0;
 }
 
+
+
+/*
+ * dcmotor2.c
+ *
+ * Created: 01-12-2021 15:44:20
+ * Author : ROHIT
+ */ 
+
+#include <avr/io.h>
+int main(void)
+{
+  DDRB=0x00;
+  PORTB=0xFF;
+  DDRC=0xFF;
+  while(1)
+  {
+if(PINB&0b00000001)//02
+{
+PORTC=0x02;   
+}
+else
+{ 
+PORTC=0x01; 
+}
+  }
+  return 0;
+}
